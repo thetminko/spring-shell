f46612a3d75d787c1f1e9171ed2256c61085d970
thetminko <dev.minko@gmail.com>
2017-02-01 22:58:37 +0800

    Added Logging

--- commandline/src/main/java/com/thetminko/spring/shell/HelloWorld.java
+++ commandline/src/main/java/com/thetminko/spring/shell/HelloWorld.java
@@ -1,37 +1,31 @@
 package com.thetminko.spring.shell;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.ExitShellRequest;
-import org.springframework.shell.core.JLineShellComponent;
-import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StopWatch;
-
-import static java.util.logging.Level.INFO;
 
 /**
  * Created by thetminko on 2/1/2017.
  */
 @Component
 public class HelloWorld implements CommandMarker {
-  @CliAvailabilityIndicator({"helloworld"})
-  public boolean isAvailableCommand() {
-    return true;
-  }
+  private static final Logger LOG = LoggerFactory.getLogger(HelloWorld.class);
 
   @CliCommand(value = "hello", help = "Hello World")
   public String sayHello(@CliOption(key = {"", "name"}) String name) throws InterruptedException {
-    return "Hello " + name  + " exit code: " + run().getExitCode();
+    if (0 == run().getExitCode()) {
+      LOG.info("Successfully finished activation of schools");
+    }
+    return "Hello " + name;
   }
 
   private ExitShellRequest run() throws InterruptedException {
-    StopWatch sw = new StopWatch("Running ...");
-    sw.start();
+    LOG.info("Running Thread to activate schools");
     Thread.sleep(500);
-    sw.stop();
-
     return ExitShellRequest.NORMAL_EXIT;
   }
 }
